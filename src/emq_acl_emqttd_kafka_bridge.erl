c961cf2478de89d51cdc6aa312e35744e5985e7c
guide16 <majiao716@163.com>
2019-01-09 15:35:35 +0800

    fix: 导出模块名称

--- src/emq_acl_emqttd_kafka_bridge.erl
+++ src/emq_acl_emqttd_kafka_bridge.erl
@@ -12,8 +12,6 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module(emqx_acl_demo).
-
 -module(emq_acl_emqttd_kafka_bridge).
 
 %% ACL callbacks
