85259874e5b3976618b5bc6b160da43ae02f813d
engxin <eingxin@163.com>
2019-03-19 12:53:32 +0800

    :bug: Fixing a bug.

--- src/emqx_kafka_bridge.erl
+++ src/emqx_kafka_bridge.erl
@@ -54,8 +54,8 @@ on_client_connected(#{client_id := ClientId, username := Username}, ConnAck, Con
     Payload = [{client_id, ClientId}, {username, Username}, {conn_ack, ConnAck}, {ts, emqx_time:now_secs(Now)}],
     Connected = proplists:get_value(connected, _Env),
     Partition = proplists:get_value(partition, _Env),
+    ?LOG(error, "client-connected: topic:~s, client_id:~s , username:~s, conn_ack:~w, conn_attrs:~p~n, Payload:~s", [Connected, ClientId, Username, ConnAck, ConnAttrs, Payload]),
     ok = brod:produce_sync(brod_client_1, Connected, getPartiton(ClientId,Partition), ClientId, Payload),
-    ?LOG(error, "client-connected: topic:~s, client_id:~s , username:~s, conn_ack:~w, conn_attrs:~p~n, ok:~s", [Connected, ClientId, Username, ConnAck, ConnAttrs, ok]),
     {ok, Payload}.
 
 on_client_disconnected(#{client_id := ClientId, username := Username}, ReasonCode, _Env) ->
