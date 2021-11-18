24b0e49bcccdd673751d754718199c3024993a3f
cshlxm <cshlxm@126.com>
2021-11-18 23:46:04 +0800

    fix bug

--- src/emqx_plugin_kafka_bridge_app.erl
+++ src/emqx_plugin_kafka_bridge_app.erl
@@ -26,7 +26,7 @@
 
 start(_StartType, _StartArgs) ->
     {ok, Sup} = emqx_plugin_kafka_bridge_sup:start_link(),
-    emqx_plugin_template:load(application:get_all_env()),
+    emqx_plugin_kafka_bridge:load(application:get_all_env()),
     {ok, Sup}.
 
 stop(_State) ->
